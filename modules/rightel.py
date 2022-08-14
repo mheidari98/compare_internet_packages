e37e6ba8b8031ce97ba5a2ed48be4640f158d7db
Mahdi Heidari <mahdih3idari@gmail.com>
2022-08-14 13:55:08 +0430

    convert package_price to english number

--- modules/rightel.py
+++ modules/rightel.py
@@ -6,6 +6,30 @@ from selenium import webdriver
 import time
 import math
 
+def fa2EnNum(number_string):
+    number_string = number_string.replace(',', '')
+    dic = {
+        '۰' : 0,
+        '۱' : 1,
+        '٢' : 2,
+        '۲' : 2,
+        '۳' : 3,
+        '٤' : 4,
+        '۴' : 4,
+        '۵' : 5,
+        '٦' : 6,
+        '۶' : 6,
+        '۷' : 7,
+        '۸' : 8,
+        '۹' : 9,
+    }
+    num = 0
+    for ch in number_string :
+        num = num*10 + dic[ch]
+
+    return num
+
+
 def CreateDriver():
     options = webdriver.ChromeOptions()
     options.add_argument("--enable-javascript")
@@ -16,6 +40,7 @@ def CreateDriver():
     driver.implicitly_wait(3)
 
     return driver
+
 
 def rightel(allow_limited_packs=False):
 
@@ -47,5 +72,11 @@ def rightel(allow_limited_packs=False):
                 ussd_code_block = ''
             df.loc[0 if pd.isnull(df.index.max()) else df.index.max() + 1] = \
                         [package_type, package_volume_info, package_price_rial, ussd_code_block]
+
+    df['package_price'] = df['package_price'].apply(lambda x: fa2EnNum(x)* 1.09) # 9% tax
+
+    df = df.drop_duplicates(keep='first').reset_index(drop=True)
+
+    df.to_csv("DB/rightel.csv", sep=',' , encoding='utf-8-sig', index=False)
 
     return df
